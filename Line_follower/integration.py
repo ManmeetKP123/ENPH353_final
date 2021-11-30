950d1701dd64132db00d1d5c0653cb46ace4a481
michellechoco <michelleecao@gmail.com>
2021-11-29 20:48:27 -0800

    combined masks

--- Line_follower/integration.py
+++ Line_follower/integration.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
 import roslib
 import sys
 import cv2
@@ -23,11 +22,9 @@ set_session(sess1)
 class image_converter:
 
 	def __init__(self):
-		self.image_pub = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=1)
 		self.bridge = CvBridge()
 		self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw", Image, self.callback)
 		self.plate_NN = models.load_model('/home/fizzer/Downloads/model.h5')
-
 
 	def callback(self, data):
 		try:
@@ -36,23 +33,35 @@ class image_converter:
 		except CvBridgeError as e:
 			print(e)
 
-		cv2.imshow("Camera feed", img)
-		cv2.waitKey(3)
-
-
-
+		cv2.imshow("Image", img)
+		cv2.waitKey(2)
 
 		pre_mask_img = cv2.medianBlur(img,5)
 		pre_mask_img = cv2.cvtColor(pre_mask_img, cv2.COLOR_BGR2HSV)
 
+		lower_hsv1 = np.array([120,135,100])
+		upper_hsv1 = np.array([120,255,105])
 
-		lower_hsv_car = np.array([120,135,100])
-		upper_hsv_car = np.array([120,255,105])
+		lower_hsv2 = np.array([120,200,120])
+		upper_hsv2 = np.array([120,255,130])
 
-		masked_img = cv2.inRange(pre_mask_img, lower_hsv_car, upper_hsv_car)
+		lower_hsv3 = np.array([120,120,130])
+		upper_hsv3 = np.array([120,255,255])
 
-		cv2.imshow("Masked feed", masked_img)
-		cv2.waitKey(3)
+		hsv_ranges = [[lower_hsv1, upper_hsv1], 
+					  [lower_hsv2, upper_hsv2],
+					  [lower_hsv3, upper_hsv3]]
+
+		masked_img = 0
+
+		for lower, upper in hsv_ranges:
+			pre_mask_img_copy = pre_mask_img.copy()
+			masked_img += cv2.inRange(pre_mask_img_copy, lower, upper)
+
+		# masked_img = cv2.inRange(pre_mask_img, lower_hsv_car, upper_hsv_car)
+
+		cv2.imshow("Masked image", masked_img)
+		cv2.waitKey(2)
 
 		contours, hierarchy = cv2.findContours(image=masked_img, mode=cv2.RETR_EXTERNAL, 
 			method=cv2.CHAIN_APPROX_SIMPLE)[-2:]
@@ -65,18 +74,10 @@ class image_converter:
 		if len(contours) >= 2:
 			smaller_contour_area = cv2.contourArea(contours[-1])
 			print(smaller_contour_area)
-			if smaller_contour_area > 1000:
-				# contour detection
-
+			if smaller_contour_area > 1500:
 				print("Entering contour detection")
 
-				# convert image to grayscale
-				# img_gray = cv2.cvtColor(mask_car, cv2.COLOR_BGR2GRAY)
-				# h, s, v = cv2.split(masked_img)
-				# img_gray = v
-				# img_gray = masked_img[:,:,2]
-
-				# # apply binary thresholding
+				# apply binary thresholding
 				ret, thresh = cv2.threshold(masked_img, 150, 255, cv2.THRESH_BINARY)
 
 				# detect contours on the binary image using cv2.CHAIN_APPROX_SIMPLE
@@ -125,11 +126,6 @@ class image_converter:
 				dst_pts = np.float32([[0, 0], [dst_width, 0], [0, dst_height], [dst_width, dst_height]])
 
 				img_copy = img.copy()
-
-				# draw contours on original image 
-				box = np.int0(box)
-				cv2.drawContours(img,[box],0,(0,0,255),2)
-
 				matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
 				result = cv2.warpPerspective(img_copy, matrix, (dst_width, dst_height))
 
@@ -224,56 +220,7 @@ class image_converter:
 
 					return predicted_license_plate
 
-				print(preprocess_input(resize_img))
-
-
-
-
-
-
-
-		# width = cv_image.shape[1]
-		# camera_center = width // 2
-
-		# bottom = cv_image[-100,:,0]
-
-		# array = []
-		# # threshold = int((np.amax(bottom) + np.amin(bottom)) / 2)
-		# threshold = 75
-
-		# for x in bottom:
-		# 	if x > threshold:
-		# 		array.append(1)
-		# 	else:
-		# 		array.append(0)
-
-		# arr = np.array(array)
-
-		# line = np.where(arr == 0)
-		# line = np.array(line)
-		# line = line[0,:]
-
-		# rate = rospy.Rate(2)
-		# move = Twist()
-		# move.linear.x = 0.5
-		# # move.angular.z = 0.2
-		# move.angular.z = 0.2
-
-		# margin = 20
-
-		# if len(line) != 0:
-		#     x = (line[0] + line[-1]) // 2
-
-		#     if x > (camera_center + margin):
-		#     	# move.angular.z = -1  # positive is left
-		#     	pass
-		#     elif x < (camera_center + margin):
-		#     	move.angular.z = 3
-		#     else: 
-		#     	move.angular.z = 0
-
-		# print(move.angular.z, move.linear.x)
-		# self.image_pub.publish(move)
+				print("Prediction:", preprocess_input(resize_img))
 
 
 def main(args):	
